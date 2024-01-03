d95401878c2e82a3db2f8778e8e1b043c16b2106
THOSSYN <alextosin5@gmail.com>
2024-01-03 10:41:17 -0800

    5-photo-reject.js and 6-final-userr.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,8 +5,19 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.all([userPromise, photoPromise]).then((values) => values.map((value) => ({
-    status: value instanceof Error ? 'rejected' : 'resolved',
-    value: value instanceof Error ? value.message : value,
-  })));
+  return Promise.all([userPromise, photoPromise])
+    .then((values) =>
+      values.map((value) => ({
+        status: value instanceof Error ? 'rejected' : 'resolved',
+        value: value instanceof Error ? value.message : value,
+      }))
+    )
+    .catch((error) => {
+      return [
+        {
+          status: 'rejected',
+          value: error instanceof Error ? error.message : error,
+        },
+      ];
+    });
 }

dc3ef1ead52c836f4e9a07031c1a1e92ee68aa80
THOSSYN <alextosin5@gmail.com>
2024-02-26 22:33:11 +0100

    Added README.md, 0-console.js and 1-stdin.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,5 @@
+const displayMessage = function(input) {
+  process.stdout.write(input + '\n');
+}
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?' + '\n');
+
+process.stdin.on('readable', function() {
+  const input = process.stdin.read();
+  if (input) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+
+process.stdin.on('end', function() {
+  process.stdout.write('This important software is now closing ' + '\n');
+});

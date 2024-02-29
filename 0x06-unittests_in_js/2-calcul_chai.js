da03672bce1822b39cf19629a22268179e53e6d2
THOSSYN <alextosin5@gmail.com>
2024-02-29 14:10:29 +0100

    Added test for 2-calcul_chai.js

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,19 @@
+function calculateNumber(type = 'SUM', a, b) {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  } else if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  } else if (type === 'DIVIDE') {
+    if (Math.round(b) !== 0) {
+      return Math.round(a) / Math.round(b);
+    } else {
+      return 'Error';
+    }
+  }
+}
+
+/*console.log(calculateNumber('SUM', 1.4, 4.5));
+console.log(calculateNumber('SUBTRACT', 1.4, 4.5));
+console.log(calculateNumber('DIVIDE', 1.4, 4.5));
+console.log(calculateNumber('DIVIDE', 1.4, 0));*/
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,55 @@
+import calculateNumber from './2-calcul_chai.js';
+import { expect } from 'chai';
+
+describe('calculateNumber', function () {
+  describe('SUM', function () {
+    it('Returns the sum of two numbers if type is SUM', function () {
+      const sum = calculateNumber(1.4, 4.5);
+      expect(sum).to.equal(6);
+    });
+
+    it('Returns the sum of two numbers if type is SUM', function () {
+      const sum = calculateNumber(0, 0);
+      expect(sum).to.equal(0);
+    });
+
+    it('Returns the sum of two numbers if type is SUM', function () {
+      const sum = calculateNumber(-1, 4);
+      expect(sum).to.equal(3);
+    });
+  });
+
+  describe('SUBTRACT', function () {
+    it('Difference of two numbers if type is SUBTRACT', function () {
+      const difference = calculateNumber(1.4, 4.5, 'SUBTRACT');
+      expect(difference).to.equal(-4);
+    });
+
+    it('Difference of two numbers if type is SUBTRACT', function () {
+      const difference = calculateNumber(0, 0, 'SUBTRACT');
+      expect(difference).to.equal(0);
+    });
+
+    it('Difference of two numbers if type is SUBTRACT', function () {
+      const difference = calculateNumber(-1, 4, 'SUBTRACT');
+      expect(difference).to.equal(-5);
+    });
+  });
+
+  describe('DIVIDE', function () {
+    it('DIVIDE a number by another if type is DIVIDE', function () {
+      const div = calculateNumber(1.4, 4.5, 'DIVIDE');
+      expect(div).to.equal(0.2);
+    });
+
+    it('DIVIDE a number by another if type is DIVIDE', function () {
+      const div = calculateNumber(1.4, 0, 'DIVIDE');
+      expect(div).to.equal('Error');
+    });
+
+    it('DIVIDE a number by another if type is DIVIDE', function () {
+      const div = calculateNumber(-1, 4, 'DIVIDE');
+      expect(div).to.equal(-0.25);
+    });
+  });
+});

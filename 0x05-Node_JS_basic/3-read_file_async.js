5ca9cf6bb9dde1b086202320264201fe27ebcd0b
THOSSYN <alextosin5@gmail.com>
2024-03-04 00:11:27 +0100

    Updated 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,33 +1,39 @@
 const fs = require('fs').promises;
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, { encoding: 'utf8' });
-    const lines = data.split('\n');
-    const lineLength = lines.length;
-    const fieldCount = {};
-    const nameList = {};
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf8' })
+      .then((data) => {
+        const lines = data.split('\n').filter((line) => line.trim() !== ''); // Exclude empty lines
+        const lineLength = lines.length;
+        const fieldCount = {};
+        const nameList = {};
 
-    for (let i = 1; i < lines.length; i++) {
-      const line = lines[i].split(',');
-      const firstName = line[0];
-      const field = line[3];
+        for (let i = 1; i < lines.length; i += 1) {
+          const line = lines[i].split(',');
+          const firstName = line[0];
+          const field = line[3];
 
-      fieldCount[field] = (fieldCount[field] || 0) + 1;
+          fieldCount[field] = (fieldCount[field] || 0) + 1;
 
-      if (!nameList[field]) {
-        nameList[field] = [];
-      }
+          if (!nameList[field]) {
+            nameList[field] = [];
+          }
 
-      nameList[field].push(firstName);
-    }
+          nameList[field].push(firstName);
+        }
 
-    console.log(`Number of students: ${lineLength - 2}`);
-    console.log(`Number of students in CS: ${nameList['CS'] ? nameList['CS'].length : 0} List: ${nameList['CS'] ? nameList['CS'].join(', ') : ''}`);
-    console.log(`Number of students in SWE: ${nameList['SWE'] ? nameList['SWE'].length : 0}. List: ${nameList['SWE'] ? nameList['SWE'].join(', ') : ''}`);
-  } catch (err) {
-    console.error('Error: Cannot load the database');
-  }
+        console.log(`Number of students: ${lineLength - 1}`);
+        console.log(`Number of students in CS: ${nameList.CS ? nameList.CS.length : 0} List: ${nameList.CS ? nameList.CS.join(', ') : ''}`);
+        console.log(`Number of students in SWE: ${nameList.SWE ? nameList.SWE.length : 0}. List: ${nameList.SWE ? nameList.SWE.join(', ') : ''}`);
+
+        resolve({ lineLength, fieldCount, nameList }); // Resolve with the data
+      })
+      .catch(() => {
+        // console.error('Error: Cannot load the database');
+        reject(new Error('Cannot load the database'));
+      });
+  });
 }
 
 module.exports = countStudents;

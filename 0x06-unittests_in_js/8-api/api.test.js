5fd5499856fd6ab1467cedaf021d5d9bc1dfc0ee
THOSSYN <alextosin5@gmail.com>
2024-03-03 18:28:32 +0100

    Recommitted 8-api folder

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,16 +1,14 @@
-const expect = require('chai').expect;
-const request = require('supertest');
+const request = require('request');
+const { expect } = require('chai');
 const app = require('./api');
 
 describe('API', function() {
   it('Should display "API available on localhost port 7865"', function(done) {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end(function(err, res) {
-        if (err) return done(err);
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+    request('http://localhost:7865/', function(err, res, body) {
+      if (err) return done(err);
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
   });
 });

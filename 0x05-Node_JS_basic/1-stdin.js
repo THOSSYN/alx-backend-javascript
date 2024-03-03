0553a864861ea070c7a1ba0372f519d0af3862a7
THOSSYN <alextosin5@gmail.com>
2024-03-03 21:47:50 +0100

    Updated 2-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
 process.stdin.setEncoding('utf-8');
 
-process.stdout.write('Welcome to Holberton School, what is your name?' + '\n');
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('readable', function() {
+process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input) {
     process.stdout.write(`Your name is: ${input}`);
   }
 });
 
-process.stdin.on('end', function() {
-  process.stdout.write('This important software is now closing ' + '\n');
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });

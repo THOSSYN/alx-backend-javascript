2bb2abecafe99b289cca003f0801d21bf39f082f
THOSSYN <alextosin5@gmail.com>
2024-03-07 09:29:07 +0100

    Correct output formatted

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,54 +1,49 @@
 const http = require('http');
 const fs = require('fs');
 
+const host = 'localhost';
+const port = 1245;
+const file = process.argv[2] || 'database.csv';
+
 const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    try {
-      const fileName = process.argv[2];
-      const data = fs.readFileSync(fileName, 'utf8');
-      const lines = data.split('\n').filter((line) => line.trim() !== ''); // Remove empty lines
-      const nameList = {};
-      const fieldCount = {};
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('This is the list of our students\nCannot load the database');
+        return;
+      }
 
-      lines.forEach((line) => {
-        const fields = line.split(',');
-        const firstName = fields[0].trim();
-        const field = fields[3].trim();
+      const studentsInfo = data.split('\n').filter((line) => line !== '');
+      studentsInfo.shift();
+      const numberOfStudents = studentsInfo.length;
+      let resInfo = `This is the list of our students
+Number of students: ${numberOfStudents}\n`;
+      const studentsInField = {};
 
-        fieldCount[field] = (fieldCount[field] || 0) + 1;
+      studentsInfo.forEach((row) => {
+        const studentInfo = row.split(',');
+        const field = studentInfo.pop();
+        const name = studentInfo.shift();
 
-        if (!nameList[field]) {
-          nameList[field] = [];
-        }
-
-        nameList[field].push(firstName);
+        if (!studentsInField[field]) studentsInField[field] = [];
+        studentsInField[field].push(name);
       });
 
-      const lineLength = lines.length - 1; // Subtract header line
-      const csStudents = nameList.CS ? nameList.CS.length : 0;
-      const sweStudents = nameList.SWE ? nameList.SWE.length : 0;
-
-      let response = 'This is the list of our students\n';
-      response += `Number of students: ${lineLength}\n`;
-      response += `Number of students in CS: ${csStudents}. List: ${nameList.CS ? nameList.CS.join(', ') : ''}\n`;
-      response += `Number of students in SWE: ${sweStudents}. List: ${nameList.SWE ? nameList.SWE.join(', ') : ''}\n`;
-
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(response);
-    } catch (err) {
-      console.error(err);
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Internal Server Error\n');
-    }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found\n');
+      for (const [field, listOfNames] of Object.entries(studentsInField)) {
+        const numStudents = listOfNames.length;
+        const names = listOfNames.join(', ');
+        const fieldInfo = `Number of students in ${field}: ${numStudents}. List: ${names}\n`;
+        resInfo += fieldInfo;
+      }
+      res.statusCode = 200;
+      res.end(resInfo.slice(0, -1));
+    });
   }
 });
 
-app.listen(1245);
-
 module.exports = app;
+app.listen(port, host);
